33f6458b4b5857202da737a2f4f69054c6061c5a
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-22 18:19:33 +0200

    Rotire

--- Laborator4/Program.cs
+++ Laborator4/Program.cs
@@ -11,7 +11,43 @@ namespace Laborator4
         static void Main(string[] args)
         {
             //MatriceSpirala1();
-            MatriceSpirala2();
+            //MatriceSpirala2();
+            Rotire();
+        }
+
+        private static void Rotire()
+        {
+            int[,] m = new int[,]
+            {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+            };
+
+            int n = m.GetLength(0), aux;
+
+            for (int i = 0; i < n/2; i++)
+            {
+                for (int j = 0; j < n/2; j++)
+                {
+                    aux = m[i, j];
+                    m[i, j] = m[n - j - 1, i];
+                    m[n - j - 1, i] = m[n - i - 1, n - j - 1];
+                    m[n - i - 1, n - j - 1] = m[j, n - i - 1];
+                    m[j, n - i - 1] = aux;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(m[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
 
         private static void MatriceSpirala2()
