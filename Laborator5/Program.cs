a4ecff2b9dd6db64202e393601d420c9a5eda75c
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-29 17:57:30 +0300

    Recursivitate

---
+++ Laborator5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursivitate();
+        }
+
+        private static void Recursivitate()
+        {
+            print("Madalina", 0);
+            Console.WriteLine();
+        }
+
+        private static void print(string str, int index)
+        {
+            if (index == str.Length)
+            {
+                return;
+            }
+            Console.Write(str[index]);
+            print(str, index + 1);
+
+            //print(str, index + 1);
+            //Console.Write(str[index]);
+        }
+    }
+}
