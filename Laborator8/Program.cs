e0d1e333a91d6ae2ecf76599ab86cd3d299671b2
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-04-26 17:37:21 +0300

    Encriptare

--- Laborator8/Program.cs
+++ Laborator8/Program.cs
@@ -13,8 +13,8 @@ namespace Laborator8
         static void Main(string[] args)
         {
             BubbleSort();
-            NumerePrime();
-            CybersecurityRSA();
+            //Encriptare();
+            //CybersecurityRSA();
         }
 
         static Stopwatch sw = new Stopwatch();
@@ -123,9 +123,93 @@ namespace Laborator8
                 return false;
         }
 
-        private static void NumerePrime()
+        private static void Encriptare()
         {
-            
+/*using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+ 
+namespace RSAEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+ 
+                return null;
+            }
+ 
+        }
+ 
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+ 
+                return null;
+            }
+ 
+        }
+ 
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plaintext = ByteConverter.GetBytes(txtplain.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            txtencrypt.Text = ByteConverter.GetString(encryptedtext);
+ 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] decryptedtex = Decryption(encryptedtext, RSA.ExportParameters(true), false);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtex);
+        }
+        private void txtplain_TextChanged(object sender, EventArgs e)
+        {
+ 
+        }
+    }
+}*/
         }
 
         private static void BubbleSort()
