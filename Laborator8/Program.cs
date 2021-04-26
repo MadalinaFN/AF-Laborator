c59f8150cea93c3ec6e0a41d448be0913f8d5497
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-04-26 18:23:11 +0300

    Fibonacci

--- Laborator8/Program.cs
+++ Laborator8/Program.cs
@@ -16,6 +16,30 @@ namespace Laborator8
             //Encriptare();
             //CybersecurityRSA();
             //MargeSort();
+            //Fibonacci();
+        }
+
+        private static void Fibonacci()
+        {
+            int p;
+            float a = 0, b = 1, x, i;
+            Console.WriteLine("Introduceti Pozitia: ");
+            p = int.Parse(Console.ReadLine());
+            int[] v = new int[p];
+            if (a == 0 && b == 1)
+            {
+                for (i = 0; i < v.Length; i++)
+                {
+                    x = a + b;
+                    a = b;
+                    b = x;
+                }
+            }
+            if (p == 0) Console.WriteLine("Numarul de pe pozitia {0} este: {1}", p, a);
+            else Console.WriteLine("Numarul de pe pozitia {0} este: {1}", p, b);
+
+
+            Console.ReadKey();
         }
 
         private static void MargeSort()
