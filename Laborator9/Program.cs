24436fd682df603a28722f074619d8b364cd7206
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-05-10 18:08:21 +0300

    SelectionSort

--- Laborator9/Program.cs
+++ Laborator9/Program.cs
@@ -12,6 +12,50 @@ namespace Laborator9
         {
             //CiurulLuiEratostene();
             //InsertionSort();
+            //SelectionSort();
+        }
+
+        private static void SelectionSort()
+        {
+            int[] a = { 4, 5, 3, 2, 4, 1 };
+            int n = a.Length;
+            stableSelectionSort(a, n);
+            printarray(a, n);
+            Console.ReadKey();
+        }
+        private static void printarray(int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(a[i] + " ");
+
+            Console.WriteLine();
+        }
+        private static void stableSelectionSort(int[] a, int n)
+        {
+            // Iterate through array elements
+            for (int i = 0; i < n - 1; i++)
+            {
+
+                // Loop invariant : Elements till
+                // a[i - 1] are already sorted.
+
+                // Find minimum element from
+                // arr[i] to arr[n - 1].
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                    if (a[min] > a[j])
+                        min = j;
+
+                // Move minimum element at current i.
+                int key = a[min];
+                while (min > i)
+                {
+                    a[min] = a[min - 1];
+                    min--;
+                }
+
+                a[i] = key;
+            }
         }
 
         private static void InsertionSort()
